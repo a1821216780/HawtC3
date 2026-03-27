bec389a14e6c7e420b86d1ca47414dced23553e1
赵子祯 <1821216780@qq.com>
2026-03-27 14:37:53 +0800

    开始开发结构动力学模块

---
+++ src/StructL/StrL_IO_Subs.h
@@ -0,0 +1,30 @@
+#pragma once
+//**********************************************************************************************************************************
+// LICENSING
+// Copyright(C) 2021, 2025  TG Team,Key Laboratory of Jiangsu province High-Tech design of wind turbine,WTG,WL,赵子祯
+//
+//    This file is part of Qahse.IO.Math
+//
+// Licensed under the Boost Software License - Version 1.0 - August 17th, 2003
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.HawtC.cn/licenses.txt
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//**********************************************************************************************************************************
+
+namespace Qahse::StrL
+{
+    static StrLInput ReadStrL_MainFile()
+    {
+
+    }
+}
